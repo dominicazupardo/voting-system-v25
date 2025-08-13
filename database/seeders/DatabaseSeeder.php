c6ea9c97df563c84ff982e22b98a69d1318e7a7d
Jay Millena <jay.millena07@gmail.com>
2025-08-14 01:34:32 +0800

    Add course and user seeder with 100 students and 2 admins

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -49,72 +49,71 @@ class DatabaseSeeder extends Seeder
         ];
 
         $admin_data = [
-            'student_no' => 'Admin02',
-            'firstname' => 'Dominic',
-            'middlename' => 'Anonymous',
-            'lastname' => 'Azupardo',
-            'course' => 'BS Computer Science',
-            'year' => '4th Year',
-            'block' => 'A',
-            'mobile_no' => '09213108252',
-            'is_approved' => true,
-            'role' => 1,
-            'email' => 'dominicazupardo@gmail.com',
-            'password' => Hash::make('Itisfinished2025^'),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ];
-
-        $student_data = [
             [
-                'student_no' => '2022-12001',
-                'firstname' => 'Jane',
-                'middlename' => 'A.',
-                'lastname' => 'Smith',
-                'course' => 'BS Information Technology',
-                'year' => '3rd Year',
-                'block' => 'B',
-                'mobile_no' => '09213108253',
-                'is_approved' => true,
-                'role' => 3,
-                'email' => 'janesmitn@gmail.com',
-                'password' => Hash::make('ccdi2025^'),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'student_no' => '2022-12002',
-                'firstname' => 'John',
-                'middlename' => 'M.',
-                'lastname' => 'Doe',
+                'student_no' => 'Admin02',
+                'firstname' => 'Dominic',
+                'middlename' => 'Anonymous',
+                'lastname' => 'Azupardo',
                 'course' => 'BS Computer Science',
                 'year' => '4th Year',
                 'block' => 'A',
-                'mobile_no' => '09213108254',
+                'mobile_no' => '09213108252',
                 'is_approved' => true,
-                'role' => 3,
-                'email' => 'johndoe@gmail.com',
-                'password' => Hash::make('ccdi2025^'),
+                'role' => 1,
+                'email' => 'dominicazupardo@gmail.com',
+                'password' => Hash::make('Itisfinished2025^'),
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'student_no' => '2022-12003',
-                'firstname' => 'Alice',
-                'middlename' => 'B.',
-                'lastname' => 'Johnson',
-                'course' => 'BS Office Administration',
-                'year' => '2nd Year',
-                'block' => 'C',
-                'mobile_no' => '09213108255',
+                'student_no' => 'ElectionChair',
+                'firstname' => 'Christian',
+                'middlename' => 'C',
+                'lastname' => 'Bernarte',
+                'course' => 'BS Computer Science',
+                'year' => '4th Year',
+                'block' => 'A',
+                'mobile_no' => '09213108252',
                 'is_approved' => true,
-                'role' => 3,
-                'email' => 'alicejohnson@gmail.com',
-                'password' => Hash::make('ccdi2025^'),
+                'role' => 1,
+                'email' => 'electionchair@gmail.com',
+                'password' => Hash::make('election2025'),
                 'created_at' => now(),
                 'updated_at' => now(),
-            ]
+            ],
         ];
+
+        // Generate 100 students with incrementing details
+        $student_data = [];
+        $password = Hash::make('ccdi2025^');
+        $courses = [
+            'BS Computer Science',
+            'BS Information Technology',
+            'BS Office Administration',
+            'ACT Programming',
+            'ACT Networking',
+            'ACT Multimedia',
+        ];
+        $years = ['1st Year', '2nd Year', '3rd Year', '4th Year'];
+        $blocks = ['A', 'B', 'C'];
+        for ($i = 1; $i <= 100; $i++) {
+            $student_data[] = [
+                'student_no' => '2022-1' . str_pad($i, 4, '0', STR_PAD_LEFT),
+                'firstname' => 'Student ' . $i,
+                'middlename' => 'Ccdi',
+                'lastname' => 'Ccdi',
+                'course' => $courses[($i - 1) % count($courses)],
+                'year' => $years[($i - 1) % count($years)],
+                'block' => $blocks[($i - 1) % count($blocks)],
+                'mobile_no' => '0917' . str_pad($i, 7, '0', STR_PAD_LEFT),
+                'is_approved' => true,
+                'role' => 3,
+                'email' => 'student' . $i . '@gmail.com',
+                'password' => $password,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ];
+        }
 
         Course::insert($course_data);
         User::insert($student_data);
